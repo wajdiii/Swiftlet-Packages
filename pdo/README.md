e3ff7c78c7eabfa62770ae0873efc3e382bd8073
Elbert Alias <elbert@alias.io>
2013-03-02 16:15:00 +1100

    Added PDO package

---
+++ pdo/README.md
@@ -0,0 +1,4 @@
+```php
+<?php
+$dbh = $this->app->getSingleton('pdo')->getHandle();
+```
---
+++ pdo/config/pdo.php
@@ -0,0 +1,10 @@
+<?php
+
+$app->setConfig('pdo', array(
+	'driver'   => 'mysql',
+	'host'     => 'localhost',
+	'database' => '',
+	'username' => '',
+	'password' => ''
+	));
+
